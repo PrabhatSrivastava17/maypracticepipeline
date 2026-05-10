d2683f9f544968d27a28b94ba8c33af2d5cb13b6
prabhats <nimishasinha843@gmail.com>
2026-05-11 01:54:09 +0530

    m

--- resources.tf
+++ resources.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "rg1" {
     name     = "rg1"
     location = "eastus"
 }
+
+resource "azurerm_storage_account" "stgprabhat1" {
+    name                     = "stgprabhat123"
+    resource_group_name      = "rg1"
+    location                 = "eastus"
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
